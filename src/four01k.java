8445737ab982c830dfa9884e2042063fb96957f3
ryan <ruderyan3@gmail.com>
2026-05-09 14:30:05 -0400

    barebones class for 401k

---
+++ src/four01k.java
@@ -0,0 +1,23 @@
+//ryan williams jr; 1373857; CSCI-125; M07
+//
+
+
+
+public class four01k extends retirementAccount {
+    int salary; //starting salary
+    int contribution; //percentage of salary contributed
+    int raise; //percentage salary increase, yearly, if any
+    int match; //percentage employer will match
+    int salaryMatch; // salary limit for employer match.
+
+
+
+
+
+
+
+    @Override
+    public void calculateReturn(){
+
+    }
+}
