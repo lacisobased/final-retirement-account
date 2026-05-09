ad6ede3b959d2d99ba9b0e97fdf9ed8b1fef488b
ryan <ruderyan3@gmail.com>
2026-05-09 14:55:54 -0400

    addition of constructor in the abstract class

--- src/retirementAccount.java
+++ src/retirementAccount.java
@@ -4,13 +4,22 @@
 abstract public class retirementAccount {
     //this will be the blueprint for the IRA and the 401k accounts
     //variables
-
     int age;
     int retAge;
     double startBal; //starting cash
     double endBal; //after everything is said and done
-    int percentage; //how much they grow
+    int percent; //how much they grow
     boolean taxable; //to determine if its Roth
+
+    //constructor to avoid future headaches
+    public retirementAccount(int age, int retAge, double startBal, double endBal, int percent, boolean taxable) {
+        this.age = age;
+        this.retAge = retAge;
+        this.startBal = startBal;
+        this.endBal = endBal;
+        this.percent = percent;
+        this.taxable = taxable;
+    }
 
 
     //methods
