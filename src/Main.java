95d0117d1daa352c531983ed28be5b2dcf2345d2
ryan <ruderyan3@gmail.com>
2026-05-09 13:37:43 -0400

    barebones template for gui

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
 //ryan williams jr; 1373857; CSCI-125; M07
 //
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("12:04pm May9 2026");
+        //set up for the GUI portion
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,400);
+        frame.setLayout(new GridLayout(5, 5, 10, 10));
+
+        //components for selecting accounts
+        JLabel selection = new JLabel("Select Account to Simulate:");
+        String[] options = {"401k", "IRA"};
+        JComboBox<String> optionBox = new JComboBox<>(options);
+
+
+
     }
-    //new edit made by oatmiilk "12:50pm may 9th 2026"
+
 }
