c94a5a9547627d50d4e15a3f120592a3a8d572fb
ryan <ruderyan3@gmail.com>
2026-05-09 21:24:35 -0400

    barebones set up for the GUI

--- src/Gui.java
+++ src/Gui.java
@@ -7,7 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-public class Gui{
+public class Gui extends JFrame {
 
     public static void main(String[] args) {
         //set up for the GUI portion
@@ -21,8 +21,20 @@ public class Gui{
         String[] options = {"401k", "IRA"};
         JComboBox<String> optionBox = new JComboBox<>(options);
 
+        //tabbed panes for the various settings
+        JTabbedPane pane = new JTabbedPane();
+        JComponent infoPan = new JPanel(new GridLayout(2, 2, 20, 20));
+        JComponent fourPan = new JPanel(new GridLayout(2, 2, 20, 20));
+        JComponent iraPan = new JPanel(new GridLayout(2, 2, 20, 20));
+
+        //pane for information, roth vs non roth accounts
+        pane.addTab("pane 1", infoPan);
+        pane.setTabComponentAt(0, new JLabel("401k vs IRA"));
 
 
+        frame.add(pane);
+
+        frame.setVisible(true);
     }
 
 }
