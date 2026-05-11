3b122bac2b85c27ebd1022bc0c2945872d8b3589
ryan <ruderyan3@gmail.com>
2026-05-11 13:25:29 -0400

    description for IRAs

--- src/Gui.java
+++ src/Gui.java
@@ -40,8 +40,10 @@ public class Gui extends JFrame {
                 "<html> \u2058 A Roth IRA is funded with after-tax dollars, allowing both growth and qualified withdrawals to remain tax-free after age 59½ and after holding the IRA for at least five years. <br/>" +
                 "<html> \u2058 You can withdraw your contributions at any time tax-free and without penalty, even if you haven’t met the five-year or age requirements."
                 );
-        JLabel ira = new JLabel("<html>IRA: <br/>" +
-                "<html>");
+        JLabel ira = new JLabel("<html>Traditional IRA: <br/>" +
+                "<html> \u2058 Traditional IRAs allow you to save on income taxes now and pay them later in retirement, when you could be in a lower tax bracket and therefore owe less in taxes. <br/>" +
+                "<html> \u2058 Any investments in your IRA grow tax-deferred until withdrawal. <br/>" +
+                "<html> \u2058 Withdrawals of earnings and deductible contributions are taxed like any other income you earn in the year they're taken, if they're qualified.");
             //adding them in order
         infoPan.add(rothI);
         infoPan.add(ira);
